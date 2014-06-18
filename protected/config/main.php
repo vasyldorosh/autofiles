560bb5d2d1651711830ec431c4f697e1f379c6f8
Vasyl Dorosh <dorosh@aiken.ua>
2014-06-18 22:25:57 +0300

    frontend

--- protected/config/main.php
+++ protected/config/main.php
@@ -50,8 +50,8 @@ return array(
 				//'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				//'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 				
-				'<makeAlias:[a-z-]{1,255}>/<modelAlias:[a-z-]{1,255}>' => 'site/model',
-				'<alias:[a-z-]{1,255}>' => 'site/make',
+				'<makeAlias:[a-z-0-9_]{1,255}>/<modelAlias:[a-z-]{1,255}>' => 'site/model',
+				'<alias:[a-z-0-9_]{1,255}>' => 'site/make',
 			),
 		),
 	
