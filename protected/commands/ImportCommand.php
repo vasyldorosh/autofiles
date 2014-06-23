3e6a0da653d5ca3173b12f5e9c4704f983c4ad42
Vasyl Dorosh <dorosh@aiken.ua>
2014-06-23 15:37:19 +0300

    photos

--- protected/commands/ImportCommand.php
+++ protected/commands/ImportCommand.php
@@ -142,8 +142,8 @@ class ImportCommand extends CConsoleCommand
 	
 	public function actionModelYearPhoto()
 	{
-		$from = 0;//Yii::app()->db->createCommand('SELECT MAX(year_id) FROM auto_model_year_photo')->queryScalar();
-		//AutoModelYearPhoto::model()->deleteAllByAttributes(array('year_id'=>$from));	
+		$from = Yii::app()->db->createCommand('SELECT MAX(year_id) FROM auto_model_year_photo')->queryScalar();
+		AutoModelYearPhoto::model()->deleteAllByAttributes(array('year_id'=>$from));	
 	
 		$criteria = new CDbCriteria();
 		$criteria->addCondition("id > {$from}");		
