6980ea97d95f62a34789f2817613a9c241b9a59c
Vasyl Dorosh <dorosh@aiken.ua>
2014-07-07 16:58:14 +0300

    0-60 times

--- protected/widgets/common/spoiler.php
+++ protected/widgets/common/spoiler.php
@@ -1,16 +1,16 @@
 			<?php $truncate = explode('[truncate]', $text)?>
-			<div class="js-<?=$data['text']?>-0"><?=$truncate[0]?></div>
+			<div class="js-<?=$text?>-0"><?=$truncate[0]?></div>
 			<?php if (isset($truncate[1])):?>
-				<div class="js-<?=$data['text']?>-1" style="display: none;"><?=$truncate[1]?></div>
-				<div><a href="#" class="js-<?=$data['text']?>">show more</a></div>
+				<div class="js-<?=$text?>-1" style="display: none;"><?=$truncate[1]?></div>
+				<div><a href="#" class="js-<?=$text?>">show more</a></div>
 				<script>
-					$('.js-<?=$data['text']?>').click(function(e){
+					$('.js-<?=$text?>').click(function(e){
 						e.preventDefault();
 						if ($(this).text() == 'show more') {
-							$('.js-<?=$data['text']?>-1').show();
+							$('.js-<?=$text?>-1').show();
 							$(this).text('show less');
 						} else {
-							$('.js-<?=$data['text']?>-1').hide();
+							$('.js-<?=$text?>-1').hide();
 							$(this).text('show more');	
 						}
 					})				
