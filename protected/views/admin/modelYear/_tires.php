e9fbe73134f2208f236b224ca6d50ea2b6da32a1
Vasyl Dorosh <dorosh@aiken.ua>
2014-12-09 21:48:55 +0200

    fix

--- protected/views/admin/modelYear/_tires.php
+++ protected/views/admin/modelYear/_tires.php
@@ -5,7 +5,7 @@
 			<?php $this->widget('ext.chosen.Chosen',array(
 				'model' => $model, 
 				'attribute' => 'post_tires', 
-				'data' => Tire::getList(),
+				'data' => Tire::model()->getList(),
 				'multiple' => true,
 				'itemUrl' => '/admin/tire/update?id=',
 				'noResults' => Yii::t('admin', 'Not found'),
@@ -24,8 +24,8 @@
 			<div class="controls" style="margin-left: 30px;">
 			<?php 	
 				$modelYearTires = $model->getPost_tires(false); 
-				foreach (AutoModelYear::getListYears($model->model_id) as $item): if($id==$model->id){continue;}?>
-				<label><input type="checkbox" name="AutoModelYear[post_tires_related][]" value="<?=$item['id']?>" <?=in_array($id, $model->post_tires_related)?'checked="checked"':''?>> <span <?=($modelYearTires==$item['tires'])?'style="font-weight: bold;"':''?>><?=$item['year']?></span> <?=(count($item['tires'])>0)?'('.count($item['tires']).')':''?></label>
+				foreach (AutoModelYear::getListYears($model->model_id) as $item): if($item['id']==$model->id){continue;}?>
+				<label><input type="checkbox" name="AutoModelYear[post_tires_related][]" value="<?=$item['id']?>" <?=in_array($item['id'], $model->post_tires_related)?'checked="checked"':''?>> <span <?=($modelYearTires==$item['tires'])?'style="font-weight: bold;"':''?>><?=$item['year']?></span> <?=(count($item['tires'])>0)?'('.count($item['tires']).')':''?></label>
 			<?php endforeach;?>
 			</div>
 	</div>
