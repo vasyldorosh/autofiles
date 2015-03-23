5c5b3c42417dca239fce5d8d3658b1bbf33a76b1
root <root@changeme.com>
2015-03-23 12:47:39 +0200

    adaptive adsense blocks

--- protected/widgets/banner/336x280.php
+++ protected/widgets/banner/336x280.php
@@ -1,10 +1,11 @@
 <div style="float:left;padding-right:10px;">
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- autofiles_336 -->
+<!-- autof_336_adapt -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:336px;height:280px"
+     style="display:block"
      data-ad-client="ca-pub-3243264408777652"
-     data-ad-slot="6215492054"></ins>
+     data-ad-slot="9876838455"
+     data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
--- protected/widgets/banner/580x400.php
+++ protected/widgets/banner/580x400.php
@@ -1,9 +1,11 @@
+<br>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- autof580 -->
+<!-- autof_580_adapt -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:580px;height:400px"
+     style="display:block"
      data-ad-client="ca-pub-3243264408777652"
-     data-ad-slot="7653258858"></ins>
+     data-ad-slot="9538761250"
+     data-ad-format="auto"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
-</script>  
+</script>
