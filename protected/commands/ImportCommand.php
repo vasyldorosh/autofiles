a6694f40192600a688617542086df5f86ac78eb8
Vasyl Dorosh <dorosh@aiken.ua>
2015-04-08 11:37:23 +0300

    fix

--- protected/commands/ImportCommand.php
+++ protected/commands/ImportCommand.php
@@ -447,7 +447,9 @@ class ImportCommand extends CConsoleCommand
 			echo $autoModelYear->id . ' - ' . $autoModelYear->year . ' ' . $autoModelYear->Model->Make->title . ' ' .  $autoModelYear->Model->title . "\n";
 		
 			$url = "http://www.autoblog.com/buy/{$autoModelYear->year}-".str_replace(array("-", " ", '&'), array("_", "_", "_"), $autoModelYear->Model->Make->title)."-".str_replace(array(" ", "-", "&"), array("+", "_", "_"), $autoModelYear->Model->title)."/specs/";
+			$url = str_replace(array('+'), array(''), $url);
 			echo "$url \n";
+			
 			$content = CUrlHelper::getPage($url, '', '');
 
 			preg_match_all('/<liclass="tools_first"><ahref="http:\/\/www.autoblog.com\/cars\-compare\?v1=(.*?)&amp;type=other">CompareCars<\/a><\/li>/', str_replace(array("\n", "\t", "\r"," "), "", $content), $matches);			
