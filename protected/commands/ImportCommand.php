d8a63eec424fb580ab695ac5159a94b2f9bc69ee
Vasyl Dorosh <dorosh@aiken.ua>
2015-07-29 11:18:57 +0400

    fix

--- protected/commands/ImportCommand.php
+++ protected/commands/ImportCommand.php
@@ -59,14 +59,10 @@ class ImportCommand extends CConsoleCommand
 			foreach ($modelYears as $modelYear) {
 				$parsedModelYearIds[] = $modelYear->id;
 			}	
-			
-			print_r($parsedModelYearIds);
-			
+		
 			if (!empty($parsedModelYearIds)) {
 				$completionIds = $this->actionCompletion($parsedModelYearIds);
-					
-				print_r($completionIds);
-					
+						
 				if (!empty($completionIds)) {
 					$this->actionCompletionDetails($completionIds);
 					$this->actionSpecs();
@@ -576,11 +572,11 @@ class ImportCommand extends CConsoleCommand
 	
 		$modelYears = AutoModelYear::model()->findAll($criteria);
 		foreach ($modelYears as $keyYear=>$autoModelYear) {
-			echo $autoModelYear->id . ' - ' . $autoModelYear->year . ' ' . $autoModelYear->Model->Make->title . ' ' .  $autoModelYear->Model->title . "\n";
+			//echo $autoModelYear->id . ' - ' . $autoModelYear->year . ' ' . $autoModelYear->Model->Make->title . ' ' .  $autoModelYear->Model->title . "\n";
 		
 			$url = "http://www.autoblog.com/buy/{$autoModelYear->year}-".str_replace(array("-", " ", '&'), array("_", "_", "_"), $autoModelYear->Model->Make->title)."-".str_replace(array(" ", "-", "&"), array("+", "_", "_"), $autoModelYear->Model->title)."/specs/";
 			//$url = str_replace(array('+'), array(''), $url);
-			echo "$url \n";
+			//echo "$url \n";
 			
 			$content = CUrlHelper::getPage($url, '', '');
 			preg_match_all('/<liclass="tools_first"><ahref="http:\/\/www.autoblog.com\/cars\-compare\?v1=(.*?)&amp;type=other">CompareCars<\/a><\/li>/', str_replace(array("\n", "\t", "\r"," "), "", $content), $matches);			
@@ -597,6 +593,11 @@ class ImportCommand extends CConsoleCommand
 						$completionIds[] = $completion->id;
 					}
 				}
+				
+				echo "MATCH OK \n";
+				
+			} else {
+				echo "MATCH ERR \n";
 			}
 		}
 		
