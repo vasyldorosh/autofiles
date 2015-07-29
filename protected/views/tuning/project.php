9f8649087fb91c649a3f656a396a35213695e0e6
Vasyl Dorosh <dorosh@aiken.ua>
2015-07-29 14:48:17 +0400

    fix

--- protected/views/tuning/project.php
+++ protected/views/tuning/project.php
@@ -72,12 +72,12 @@
 				<td><a href="<?=Tire::url(array(
 					'section_width' => $project['rear_tire_section_width'],
 					'aspect_ratio' 	=> $project['rear_tire_aspect_ratio'],
-					'rim_diameter' 	=> $project['rear_rim_diameter'],
+					'rim_diameter' 	=> $project['rim_diameter'],
 					'vehicle_class'	=> $project['rear_tire_vehicle_class'],
 				))?>"><?=Tire::format(array(
 					'section_width' => $project['rear_tire_section_width'],
 					'aspect_ratio' 	=> $project['rear_tire_aspect_ratio'],
-					'rim_diameter' 	=> $project['rear_rim_diameter'],
+					'rim_diameter' 	=> $project['rim_diameter'],
 					'vehicle_class'	=> $project['rear_tire_vehicle_class'],
 				), true)?></a></td>	
 			</tr>
