6932f606b2df3f53e6722d4edb2939f147a04f59
root <root@changeme.com>
2015-08-11 17:20:39 +0300

    fix

--- protected/widgets/banner/336x280.php
+++ protected/widgets/banner/336x280.php
@@ -1,9 +1,9 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- autofiles_300x250 -->
+<!-- auto_336 -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:250px"
+     style="display:inline-block;width:336px;height:280px"
      data-ad-client="ca-pub-3243264408777652"
-     data-ad-slot="9434056455"></ins>
+     data-ad-slot="1587195250"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
