5bbc4473ec91da667e079740e9ff440578afa796
Vasyl Dorosh <dorosh@aiken.ua>
2015-08-14 20:02:05 +0400

    tire

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -398,15 +398,14 @@ ORDER BY c DESC")->queryAll();
 			$dataModeYear[$modelYear->model_id][$modelYear->year] = $modelYear->id;
 		}
 		
-		d($data);
-		
 		foreach ($data as $model_id=>$tires) {
 			foreach ($tires as $tire_key=>$years) {
 				sort($years);
 				$data[$model_id][$tire_key] = $years;
 			}			
 		}
-
+		d($data);	
+			
 		$searchData = array();
 		foreach ($data as $model_id=>$tires) {
 			foreach ($tires as $tire_key=>$years) {
