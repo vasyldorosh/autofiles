e49dbd604ed6956e89ae1a470949a05466d52ddf
Vasyl Dorosh <dorosh@aiken.ua>
2015-08-17 20:45:20 +0400

    page wheels.html - or

--- protected/views/wheels/make.php
+++ protected/views/wheels/make.php
@@ -27,11 +27,13 @@
 				<a title="<?=$make['title']?> <?=$dataModel['title']?> rims for sale, factory wheel size, bolt pattern, offset" href="/wheels<?=$dataModel['url']?>"><?=$make['title']?> <?=$dataModel['title']?> wheels</a>
 			</h3>
 			<?php if (isset($dataModelsWheels[$dataModel['id']]) && $dataWheels=$dataModelsWheels[$dataModel['id']]):?>
-					<ul class="make__vehicle-specs">
 					<?php 
 						$rwd_to = array();
 						$rwd_from = array();
 						$or = array();
+						$p_rwd_to = array();
+						$p_rwd_from = array();
+						$p_or = array();
 						
 						if (!empty($dataWheels['trd_min'])) { $rwd_from[] = $dataWheels['trd_min'];}
 						if (!empty($dataWheels['trw_min'])) { $rwd_from[] = $dataWheels['trw_min'];}
@@ -41,18 +43,34 @@
 						
 						if (!empty($dataWheels['or_min'])) { $or[] = $dataWheels['or_min'];}
 						if (!empty($dataWheels['or_max'])) { $or[] = $dataWheels['or_max'];}
+						
+						if (!empty($dataWheels['p_rd_min'])) { $p_rwd_to[] = $dataWheels['p_rd_min'];}
+						if (!empty($dataWheels['p_rw_min'])) { $p_rwd_to[] = $dataWheels['p_rw_min'];}
+						
+						if (!empty($dataWheels['p_rd_max'])) { $p_rwd_from[] = $dataWheels['p_rd_max'];}
+						if (!empty($dataWheels['p_rw_max'])) { $p_rwd_from[] = $dataWheels['p_rw_max'];}
+						
+						if (!empty($dataWheels['p_or_min'])) { $p_or[] = $dataWheels['p_or_min'];}
+						if (!empty($dataWheels['p_or_max'])) { $p_or[] = $dataWheels['p_or_max'];}
+							
 					?>	
+					<ul class="make__vehicle-specs">
 					<?php if (!empty($rwd_from) || !empty($rwd_to)):?>	
 						<li>Stock rim width <?=implode('x', $rwd_from)?><?php if(!empty($rwd_to) && $rwd_to!==$rwd_from):?><?=(!empty($rwd_from) && !empty($rwd_to))?' &ndash; ':''?><?=implode('x', $rwd_to)?><?php endif;?></li>
 					<?php endif;?>	
 					
 					<?php if (!empty($or)):?>
 						<li>Offset range <?=implode(' &ndash; ', $or)?></li>
-					</ul>
 					<?php endif;?>
+					</ul>
+					
 					<ul class="make__vehicle-specs">
-						<li>Custom rim width 17x7.5 &ndash; 20x10.0</li>
-						<li>Offset range 20 &ndash; 45</li>
+					<?php if (!empty($p_rwd_from) || !empty($p_rwd_to)):?>	
+						<li>Custom rim width <?=implode('x', $p_rwd_from)?><?php if(!empty($p_rwd_to) && $p_rwd_to!==$p_rwd_from):?><?=(!empty($p_rwd_from) && !empty($p_rwd_to))?' &ndash; ':''?><?=implode('x', $p_rwd_to)?><?php endif;?></li>
+					<?php endif;?>						
+					<?php if (!empty($p_or)):?>
+						<li>Offset range <?=implode(' &ndash; ', $p_or)?></li>
+					<?php endif;?>
 					</ul>
 			<?php endif;?>
 		</li>
