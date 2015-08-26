c964058e2d2d3f1e13d3398e099f8238fbb8502a
Vasyl Dorosh <dorosh@aiken.ua>
2015-08-26 16:53:38 +0400

    fix

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -643,7 +643,7 @@ class Project extends CActiveRecord
 			$sql = "SELECT
 						count(*) AS c,
 						CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8),
-						(SELECT MIN(ror.value)  
+						(SELECT SUM(ror.value)  
 							FROM project AS pp
 							LEFT JOIN rim_offset_range AS ror ON pp.rim_offset_range_id = ror.id
 							WHERE pp.id IN(CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8)) AND pp.rim_offset_range_id IS NOT NULL
