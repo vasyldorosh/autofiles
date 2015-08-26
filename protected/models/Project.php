d01d7f45243c5ddf5131df8d0b219f7f50bccd23
Vasyl Dorosh <dorosh@aiken.ua>
2015-08-26 17:16:24 +0400

    fix

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -651,7 +651,7 @@ class Project extends CActiveRecord
 						(SELECT MAX(ror.value)  
 							FROM project AS pp
 							LEFT JOIN rim_offset_range AS ror ON pp.rim_offset_range_id = ror.id
-							WHERE pp.id IN(CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8)) AND pp.rim_offset_range_id IS NOT NULL
+							WHERE CONCAT(',', pp.id, ',') LIKE @ids AND pp.rim_offset_range_id IS NOT NULL
 						) AS ror_max,						
 						(SELECT MIN(rear_ror.value)  
 							FROM project AS pp
