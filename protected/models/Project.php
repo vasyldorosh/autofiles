2e97dec45302739b362bd52f55cfe48c2ab9801b
Vasyl Dorosh <dorosh@aiken.ua>
2015-08-26 16:39:06 +0400

    fix

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -641,28 +641,12 @@ class Project extends CActiveRecord
 		if ($data === false || true) {
 			$data = '';
 			$sql = "SELECT
-						count(*) AS c, 
 						CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8),
 						(SELECT MIN(ror.value)  
 							FROM project AS pp
 							LEFT JOIN rim_offset_range AS ror ON pp.rim_offset_range_id = ror.id
 							WHERE pp.id IN(CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8)) AND pp.rim_offset_range_id IS NOT NULL
 						) AS ror_min,			 
-						(SELECT MAX(ror.value)  
-							FROM project AS pp
-							LEFT JOIN rim_offset_range AS ror ON pp.rim_offset_range_id = ror.id
-							WHERE pp.id IN(CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8)) AND pp.rim_offset_range_id IS NOT NULL
-						) AS ror_max,						
-						(SELECT MIN(rear_ror.value)  
-							FROM project AS pp
-							LEFT JOIN rim_offset_range AS rear_ror ON pp.rear_rim_offset_range_id = rear_ror.id
-							WHERE pp.id IN(CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8))
-						) AS rear_ror_min,			 
-						(SELECT MAX(rear_ror.value)  
-							FROM project AS pp
-							LEFT JOIN rim_offset_range AS rear_ror ON pp.rear_rim_offset_range_id = rear_ror.id
-							WHERE pp.id IN(CAST( GROUP_CONCAT(p.id) AS CHAR(10000) CHARACTER SET utf8))
-						) AS rear_ror_max,						
 						p.is_staggered_wheels AS is_staggered,
 						rd.value AS rim_diameter, 
 						rear_rd.value AS rear_rim_diameter, 
