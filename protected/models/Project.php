ec1493947ed3f1decd07e89d192538b6677f85b7
Vasyl Dorosh <dorosh@aiken.ua>
2015-09-01 11:58:07 +0400

    fix

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -943,7 +943,7 @@ class Project extends CActiveRecord
 		$key = Tags::TAG_PROJECT . '_getRecommendedTireSizes_'. $diametr_id . '_' . $width . '_' . $vehicle_class_id;
 		$tires = Yii::app()->cache->get($key);
 		
-		if ($tires === false || 1) {
+		if ($tires === false) {
 			
 			$tires = array();
 			$sql = "
@@ -978,7 +978,7 @@ class Project extends CActiveRecord
 		$key = Tags::TAG_PROJECT . '_getRecommendedTireSizes_p_'. $diametr_id . '_' . $width . '_' . $vehicle_class_id;
 		$counters = Yii::app()->cache->get($key);
 		
-		if ($counters === false || 1) {
+		if ($counters === false) {
 			
 			$counters = array();
 			if (!empty($sa)) {
