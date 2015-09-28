9e934d0ba4dd3b5c2e7a62937f2390276ad07d43
root <root@changeme.com>
2015-09-28 16:01:53 +0300

    fix

--- protected/widgets/banner/336x280.php
+++
@@ -1,9 +0,0 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- auto_336 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:336px;height:280px"
-     data-ad-client="ca-pub-3243264408777652"
-     data-ad-slot="1587195250"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
