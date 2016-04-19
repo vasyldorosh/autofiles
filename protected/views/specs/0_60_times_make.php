5f56a491581830efe73938ae670f31552478353b
Vasyl Dorosh <dorosh@aiken.ua>
2016-04-19 22:27:41 +0300

    fix

--- protected/views/specs/0_60_times_make.php
+++ protected/views/specs/0_60_times_make.php
@@ -37,7 +37,7 @@
 							<?php if ($item['mile_time']['min'] == $item['mile_time']['max']):?>
 								<?=$item['mile_time']['min']?> @ <?=$item['mile_speed']['min']?> mph
 							<?php else:?>
-								<?=$item['mile_time']['min']?> @ <?=$item['mile_speed']['min']?> - <?=$item['mile_time']['max']?> @ <?=$item['mile_speed']['max']?> mph
+								<?=$item['mile_time']['min']?> @ <?=$item['mile_speed']['max']?> - <?=$item['mile_time']['max']?> @ <?=$item['mile_speed']['min']?> mph
 							<?php endif;?>	
 						<?php endif;?>
 					</td>
